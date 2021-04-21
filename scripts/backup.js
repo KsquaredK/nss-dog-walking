84c41e4849fc069fc0759e4eee2c4995dd182963
KsquaredK <ksquaredkelly@gmail.com>
2021-04-21 15:56:38 -0400

    first debugging exercise

---
+++ scripts/backup.js
@@ -0,0 +1,10 @@
+// assignments.js lines 21-23:
+// export const Assignments = () => {
+//    let assignmentHTML = ""
+//  assignmentHTML = "<ul>"
+
+
+
+// Walkers.js lines 6-7
+//export const Walkers = () => {
+//  let walkerHTML = "<ul>"
