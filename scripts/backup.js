1c2230bac29c3b36ae8efbb112522878027c45fb
KsquaredK <ksquaredkelly@gmail.com>
2021-04-22 12:17:14 -0400

    DeShawn's debugged Dogwalking

--- scripts/backup.js
+++ scripts/backup.js
@@ -8,3 +8,10 @@
 // Walkers.js lines 6-7
 //export const Walkers = () => {
 //  let walkerHTML = "<ul>"
+
+<
+section class = "detail--column list details__cities" >
+    <
+    h2 > Pets < /h2>
+$ { Pets() } <
+/section>
